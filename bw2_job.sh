9dd9f1a2f21c2e5e630e06b0fb31c273ebd5f636
wdiazcarballo <papong@tu.ac.th>
2024-08-03 20:28:21 +0700

    new file:   .gitignore 	new file:   bw2.c 	new file:   bw2_job.sh

---
+++ bw2_job.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=bw_test
+#SBATCH --partition=helios
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=01:00:00
+#SBATCH --output=bw_test_%j.log
+#SBATCH --chdir=/global/home/users/rdmaworkshop08/wdc/rdma
+
+# Load necessary modules
+module load infiniband
+
+# Get the hostnames
+NODELIST=($(scontrol show hostnames $SLURM_JOB_NODELIST))
+SERVER_NODE=${NODELIST[0]}
+CLIENT_NODE=${NODELIST[1]}
+
+HCA="-d mlx5_0 -i 1"
+BINDS="numactl --cpunodebind=2 "
+BINDC="numactl --cpunodebind=2 "
+
+# Start the server on the first node
+srun --nodes=1 --ntasks=1 --nodelist=$SERVER_NODE ./server &
+
+# Give the server a moment to start
+sleep 5
+
+# Start the client on the second node
+srun --nodes=1 --ntasks=1 --nodelist=$CLIENT_NODE ./client $SERVER_NODE
+
+wait

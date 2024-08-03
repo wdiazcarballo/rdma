7bc7db24e9a547b31937dfc2d1c5ded17051aa42
wdiazcarballo <papong@tu.ac.th>
2024-08-03 19:55:30 +0700

    make the slurm script to launch with sbatch

--- test.sh
+++ test.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
+#SBATCH --job-name=bw_test
+#SBATCH --partition=helios
+#SBATCH --nodes=2
+#SBATCH --ntasks=2
+#SBATCH --exclusive
+#SBATCH --time=01:00:00
+#SBATCH --output=bw_test_%j.log
 
-# Configuration
+# Load necessary modules (if required)
+# module load <module-name>
+
+# Define a timestamp for the log files
 TAG=$(date +%Y%m%d-%H%M%S)
-DEST=helios007
-SRC=helios008
 
+# Get the list of allocated nodes
+NODELIST=$(scontrol show hostnames $SLURM_JOB_NODELIST)
+NODES=($NODELIST)
+
+# Assign the first node as the server and the second as the client
+SERVER_NODE=${NODES[0]}
+CLIENT_NODE=${NODES[1]}
+
+# Define the options for the server and client commands
 HCA="-d mlx5_0 -i 1"
-BINDS="numactl --cpunodebind=2 "
-BINDC="numactl --cpunodebind=2 "
+BINDS="numactl --cpunodebind=2"
+BINDC="numactl --cpunodebind=2"
 
-SERVER_PORT=18515
-MESSAGE_SIZE=4096
-MTU=1024
-RX_DEPTH=500
-ITERS=1000
-SERVICE_LEVEL=0
-GID_IDX=-1
+# Define the flags for latency and bandwidth tests
+LTFLAGS="-a $HCA -F"
+BWFLAGS="-a $HCA --report_gbits -F"
 
-# Copy server executable to remote machine
-scp ./server $DEST:~/wdc/server
+# Start the server on the server node
+srun -N1 -w $SERVER_NODE $BINDS ./server &
 
-# Start the server on local machine
-$BINDS ./server -p $SERVER_PORT -d mlx5_0 -i 1 -s $MESSAGE_SIZE -m $MTU -r $RX_DEPTH -n $ITERS -l $SERVICE_LEVEL -g $GID_IDX &
-LOCAL_SERVER_PID=$!
-echo "Server started on local machine, waiting for connections..."
-
-# Start the server on remote machine
-ssh $DEST "$BINDC ~/wdc/server -p $SERVER_PORT -d mlx5_0 -i 1 -s $MESSAGE_SIZE -m $MTU -r $RX_DEPTH -n $ITERS -l $SERVICE_LEVEL -g $GID_IDX" &
-REMOTE_SERVER_PID=$!
-echo "Server started on remote machine $DEST"
-
-# Wait for a few seconds to ensure the server is up and running
+# Wait a bit to ensure the server starts before the client tries to connect
 sleep 5
 
-# Start the client on local machine connecting to remote server
-$BINDS ./client $DEST -p $SERVER_PORT -d mlx5_0 -i 1 -s $MESSAGE_SIZE -m $MTU -r $RX_DEPTH -n $ITERS -l $SERVICE_LEVEL -g $GID_IDX
-echo "Client started, connecting to server at $SRC"
-
-# Wait for the client and server to finish
-wait $LOCAL_SERVER_PID
-wait $REMOTE_SERVER_PID
-
-echo "RDMA communication test completed."
-
+# Start the client on the client node and connect to the server
+srun -N1 -w $CLIENT_NODE $BINDC ./client $SERVER_NODE 2>&1 | tee log-bw-$TAG.txt
